f3cbfd283fbcc12cd4ae928a3c5d9b59d8da4cfc
liuzeyafzy <liuzeyafzy@gmail.com>
2016-04-06 19:58:57 +0800

    动态支持批量压缩js；动态支持批量预编译less。

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt){
   //require('time-grunt')(grunt); 如果要使用 time-grunt 插件
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     connect: {
       options: {
         port: 9000,
@@ -20,52 +21,96 @@ module.exports = function(grunt){
         }
       }
     },
+    uglify: {
+        options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'//添加banner
+        },
+        build: {//任务一：压缩a.js，不混淆变量名，保留注释，添加banner和footer
+            options: {
+                mangle: false, //不混淆变量名
+                preserveComments: 'all', //不删除注释，还可以为 false（删除全部注释），some（保留@preserve @license @cc_on等注释）
+                footer:'\n/*! <%= pkg.name %> 最后修改于： <%= grunt.template.today("yyyy-mm-dd") %> */'//添加footer
+            },
+            files: [{
+                expand: true,
+                cwd: 'app/js',
+                src: ['**/*.js', '!**/*.min.js'],
+                dest: 'app/js',
+                ext: '.min.js'
+            }]
+        },
+        release: {//任务四：合并压缩a.js和b.js
+            files:[{
+                src: ['app/js/*.min.js'],
+                dest: 'app/js-min/all.min.js'
+            }]
+        }
+    },
     less: {
       development: {
-        options: {
-          paths: ["assets/css"]
-        },
-        files: {
-          "app/css/index.css": "app/less/index.less"
-        }
+          options: {
+              paths: ["app/css"]
+          },
+          files: [{
+              expand: true,
+              cwd: 'app/less',
+              src: ['**/*.less'],
+              dest: 'app/css',
+              ext: '.css'
+          }]
+          // files: {
+          //   "app/css/index.css": "app/less/index.less"
+          // }
       }
-  },
+    },
     watch: {
-        client: {
-            files: ['app/less/index.less'],
+        less: {
+            files: ['app/less/*.less'],
             tasks: ['less'],
             options: {
                 livereload: true
             }
         },
-      livereload: {
-        options: {
-          livereload: '<%=connect.options.livereload%>'  //监听前面声明的端口  35729
-        },
-        script:{
-            files: ['app/less/{,*/}*.less'],
-            tasks: ['less:development'],
+        uglify: {
+            files: ['app/js/*.js', '!app/js/*.min.js'],
+            tasks: ['uglify'],
             options: {
-                spawn: false
+                livereload: true
             }
         },
-        files: [  //下面文件的改变就会实时刷新网页
-          'app/*.html',
-          'app/less/{,*/}*.less',
-          'app/style/{,*/}*.css',
-          'app/scripts/{,*/}*.js',
-          'app/images/{,*/}*.{png,jpg}'
-        ]
-      }
+        livereload: {
+            options: {
+                livereload: '<%=connect.options.livereload%>'  //监听前面声明的端口  35729
+            },
+            script:{
+                files: ['app/less/{,*/}*.less'],
+                tasks: ['less:development'],
+                options: {
+                    spawn: false
+                }
+            },
+            files: [  //下面文件的改变就会实时刷新网页
+              'app/*.html',
+              'app/less/{,*/}*.less',
+              'app/style/{,*/}*.css',
+              'app/js/{,*/}*.js',
+              'app/images/{,*/}*.{png,jpg}'
+            ]
+        }
     }
   });
 
   grunt.registerTask('serve', [
-    'connect:server',
-    'watch:livereload'
+      'connect:server',
+      'watch:livereload'
   ]);
   grunt.registerTask('doless', [
       'less',
-    'watch:client'
+      'watch:less'
+  ]);
+  grunt.registerTask('douglify', [
+      'uglify:build',
+      'uglify:release',
+      'watch:uglify'
   ]);
 }
